d6a0ad5ffd26f599003be54a9e7b009711878474
Kevin Lin <kevin.lin@zuhlke.com>
2021-12-06 15:27:27 +0800

    Pick a image and display

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import logo from './asset/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri});
+  }
+
+  const renderImage = () => {
+    if (selectedImage !== null) {
+      return <Image source={{uri: selectedImage.localUri}} style={styles.thumbnail}/>
+    } else {
+      return <Image source={logo} style={styles.logo}/>
+    }
+  }
+
   return (
       <View style={styles.container}>
-        <Image source={logo} style={styles.logo} />
+        {renderImage()}
         <Text style={styles.instructions}>
           To share a photo from your phone with a friend, just press the button below!
         </Text>
         <StatusBar style="auto"/>
+
+        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
       </View>
   );
 }
@@ -32,4 +65,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginHorizontal: 15,
   },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
